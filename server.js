e309b61f7f4588b687c4e2dfee082397b2593ef6
Brian <henninb@gmail.com>
2026-06-11 12:45:36 -0500

    updates

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3300;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    const requestHeaders = req.get('Access-Control-Request-Headers');
+    if (requestHeaders) {
+      res.setHeader('Access-Control-Allow-Headers', requestHeaders);
+    }
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.static(path.join(__dirname)));
 
 app.listen(port, () => {
